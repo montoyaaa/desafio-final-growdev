95345916630d3b4d1a3167844a7cdbcb4fcadbea
Ricardo Montoya <ricardomontoya2002@hotmail.com>
2020-10-14 23:09:17 -0300

    add style-loader

--- src/App.js
+++ src/App.js
@@ -2,13 +2,10 @@ import React from 'react';
 
 import Routes from './routes';
 
-import GlobalStyle from './styles/global';
-
 function App() {
     return (
         <>
             <Routes />
-            <GlobalStyle />
         </>
     );
 }
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import './App.css';
+
 import Main from './pages/Main';
 import Repository from './pages/Repository';
 
